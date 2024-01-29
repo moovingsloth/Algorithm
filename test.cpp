eb04b807f30d7c810c2f96bff82c4e8de854e46b
Lee DongWon <dlehddnjs245@khu.ac.kr>
2024-01-29 11:27:24 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main(void){
-    int arr[1][11];
-    fill(arr[0], arr[0] + 11, 0);
-    for(int i = 0; i < 1; i++){
-        for(int j = 0; j < 11; j++){
-            cout << arr[i][j] << " ";}
-        cout << "\n";
-    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
 }
