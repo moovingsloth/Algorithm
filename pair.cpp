cf21849d2560f0686586579eb12b180245b01d6d
Lee DongWon <dlehddnjs245@khu.ac.kr>
2024-03-06 13:44:26 +0900

    2024-03-06

---
+++ pair.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    pair<int, int> t1 = make_pair(10, 13);
+    pair<int, int> t2 = {4, 6};
+    cout << t2.first << ' ' << t2.second << "\n";
+    if(t2 < t1)   cout <<  "t2 < t1";;
+
+}
