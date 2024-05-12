50baf7d27dd8c1351a18507028c462a6bd8ac3d1
moovingsloth <dlehddnjs245@khu.ac.kr>
2024-05-13 02:46:42 +0900

    2024-05-13

---
+++ 1009.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int a, b, result = 1;
+        cin >> a >>b;
+        for(int j = 0; j < b; j++){
+            result = (result*a) % 10;
+        }
+        if(result == 0) {cout << 10 << endl;}
+        else{cout << result << '\n';}
+    }
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin, str);
+    
+    string s = to_string(20);
+    cout << s;
+
+}
